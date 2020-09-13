c5ed5a0efdd38f5b81b5510891ea517a9bc7278a
Tanya Kobeleva <kotadmi@gmail.com>
2020-09-14 01:47:34 +0300

    imit

--- src/ru/kotadmi/Laba1/Pr2/Ex4/Dog.java
+++ src/ru/kotadmi/Laba1/Pr2/Ex4/Dog.java
@@ -1,4 +1,40 @@
 package ru.kotadmi.Laba1.Pr2.Ex4;
 
 public class Dog {
+
+    private String name;
+    private int age;
+
+    public Dog(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public int toHumanAge() {
+        return age * 7;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Dog {" +
+                "name = '" + name + '\'' +
+                ", age = " + age +
+                '}';
+    }
 }
