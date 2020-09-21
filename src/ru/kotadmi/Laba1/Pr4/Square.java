d9da64e34a941ec3818406c7b56fbd8af953127d
Tanya Kobeleva <kotadmi@gmail.com>
2020-09-21 03:23:23 +0300

    pr 4 done

---
+++ src/ru/kotadmi/Laba1/Pr4/Square.java
@@ -0,0 +1,44 @@
+package ru.kotadmi.Laba1.Pr4;
+
+public class Square extends Rectangle {
+    protected double side;
+
+    public Square() {
+    }
+
+    public Square(double side) {
+        this.side = side;
+    }
+
+    public Square(String color, boolean filled, double side) {
+        super(color, filled);
+        this.side = side;
+    }
+
+    public double getSide() {
+        return side;
+    }
+
+    public void setSide(double side) {
+        this.side = side;
+    }
+
+    @Override
+    public void setWidth(double side) {
+        this.width = side;
+    }
+
+    @Override
+    public void setLength(double side) {
+        this.length = side;
+    }
+
+    @Override
+    public String toString() {
+        return "Square {" +
+                "side = " + side +
+                ", color = '" + color + '\'' +
+                ", filled = " + filled +
+                '}';
+    }
+}
